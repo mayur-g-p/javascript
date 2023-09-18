8b7f99abd3204312ccd8e3df4d69b6a048a4ae2c
mayur-g-p <112608013+mayur-g-p@users.noreply.github.com>
2023-09-18 18:09:48 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144451;
+let accountEmail = "mayur@gmail.com";
+let accountPassword = "12345"
+
+accountCity: "jaipur" 
+
+// accountId=123; // not allowed
+accountEmail ="ma@ma.com"
+accountPassword ="345"
+accountCity= "bengaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
