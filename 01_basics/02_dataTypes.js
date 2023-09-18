9d384d357363851dac40f6d6826b991c1e0fd43b
mayur-g-p <112608013+mayur-g-p@users.noreply.github.com>
2023-09-18 18:27:46 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+
+let name ="mayur"
+let age = 18
+let isLoggedIn = false
+let state ;
+
+//number
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined => value is not assign
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
