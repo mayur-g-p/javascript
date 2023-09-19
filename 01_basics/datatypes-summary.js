9266dc5396a6145382349912780462dfb3a5bd52
mayur-g-p <112608013+mayur-g-p@users.noreply.github.com>
2023-09-19 13:09:39 +0000

    data types primitive and non primitive

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -25,3 +25,35 @@ const myFunc = function (){
     console.log("hello")
 }
 myFunc()
+
+
+//++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive ), Heap(Non - Primitive)
+let myYoutubename = "mayur"
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+ let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+ }
+
+ let userTwo = userOne
+
+ userTwo.email= "mayur@google.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
+
+
+
+
+
+
+
